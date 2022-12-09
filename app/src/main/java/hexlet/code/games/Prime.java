5b53447c3aa60ec1dc059765cfcf9c89ff2cdbcc
DPolkovnikov <Dpolkovnikov@alfabank.ru>
2022-12-09 11:31:56 +0100

    fix bug

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -19,7 +19,7 @@ public class Prime {
         if (number == 1) {
             return false;
         }
-        for (int i = 1; i < number / 2 + 1; i++) {
+        for (int i = 2; i < number / 2 + 1; i++) {
             if (number % i == 0) {
                 return false;
             }
